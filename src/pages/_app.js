748297d20f8505c90be5364ba089c3872976dc40
kahnu044 <kahnucharan044@gmail.com>
2025-01-01 00:09:52 +0530

    NoobAuth Updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Welcome To NoobAuth Client App</title>
+        <link rel="icon" href="/noobauth.png" sizes="any" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import axios from "axios";
 
 export default function Home() {
   const router = useRouter();
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-
     console.log("NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL);
     // Extract token from URL query parameters
     const urlParams = new URLSearchParams(window.location.search);
-    const authToken = urlParams.get('token');
+    const authToken = urlParams.get("token");
 
     if (authToken) {
       setToken(authToken);
-      localStorage.setItem('authToken', authToken);
-      router.push('/dashboard'); // Redirect to dashboard after successful login
+      localStorage.setItem("authToken", authToken);
+      router.push("/dashboard"); // Redirect to dashboard after successful login
     }
   }, []);
 
@@ -26,21 +26,33 @@ export default function Home() {
   };
 
   const handleLogout = async () => {
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     setToken(null);
     window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/logout`;
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Next.js Client App</h1>
+    <div className="flex min-h-screen items-center justify-center flex-col">
+      <img src="/noobauth.png" alt="NoobAuth" className="w-20 h-20 mb-10" />
+      <h1 className="text-light text-lg md:text-3xl ">
+        Welcome to <span className="text-[#748ef6]"> NoobAuth </span> Next.js
+        Client App
+      </h1>
+
       {!token ? (
         <>
           <button
             onClick={handleLogin}
-            style={{ padding: '10px 20px', marginTop: '20px', cursor: 'pointer', fontSize: '16px' }}
+            className="bg-[#748ef6] text-light px-2 py-1 mt-8 rounded-md "
           >
-            Login with Noob Auth
+            <span className="flex items-center justify-center font-bold">
+              <img
+                src="/noobauth.png"
+                alt="NoobAuth"
+                className="w-10 h-10 mr-2"
+              />
+              Login with Noob Auth
+            </span>
           </button>
         </>
       ) : (
@@ -48,7 +60,12 @@ export default function Home() {
           <p>You are logged in!</p>
           <button
             onClick={handleLogout}
-            style={{ padding: '10px 20px', marginTop: '20px', cursor: 'pointer', fontSize: '16px' }}
+            style={{
+              padding: "10px 20px",
+              marginTop: "20px",
+              cursor: "pointer",
+              fontSize: "16px",
+            }}
           >
             Logout
           </button>
